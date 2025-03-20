a98254996024849739c5f796a342ae4d71a8b5fa
Carlos Espinosa <carloespinosa88@gmail.com>
2025-03-19 19:49:29 -0500

    Add the navbar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,10 @@
 "use client";
 
 import Providers from '@/components/Providers';
-import { Geist, Geist_Mono, Montserrat } from "next/font/google";
+import Navbar from '@/components/Navbar'
+import { geistSans, geistMono, montserrat } from '@/fonts'
 import "./globals.css";
-import Navbar from '@/components/Navbar';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-const montserrat = Montserrat({
-  variable: "--font-montserrat",
-  subsets: ["latin"],
-});
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
