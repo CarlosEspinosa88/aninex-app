295462b1b75f91ce294c2bbcf325516e6d97f057
Carlos Espinosa <carloespinosa88@gmail.com>
2025-04-07 15:39:30 -0500

    Test fixes

--- src/tests/pages/Home.test.tsx
+++ src/tests/pages/Home.test.tsx
@@ -1,14 +1,9 @@
-import { 
-  fireEvent,
-  screen, 
-  waitFor 
-} from '@testing-library/react';
+import { fireEvent, screen, waitFor } from '@testing-library/react';
 import { renderWithProviders } from '@/utils/test-utils';
+import { getAnimesMock } from '@/mocks/dataMock';
 import AnimesPage from '@/app/page';
-import { getAnimesMock, getAnimesFilteredMock } from '@/mocks/dataMock';
 
 describe('Animes list', () => {
-  
   it('should render loading state', async () => {
     renderWithProviders(<AnimesPage />, {
       mocks: getAnimesMock,
@@ -17,7 +12,7 @@ describe('Animes list', () => {
     expect(screen.getByText(/loading page/i)).toBeInTheDocument();
   });
 
-  it('should render current season or popular animes', async () => {
+  it('should render current season and popular animes', async () => {
     renderWithProviders(<AnimesPage />, {
       mocks: getAnimesMock
     });
@@ -33,18 +28,16 @@ describe('Animes list', () => {
 
   it('should render filtered anime', async () => {
     renderWithProviders(<AnimesPage />, {
-      mocks: getAnimesFilteredMock
+      mocks: getAnimesMock
     });
 
     const searchInput = screen.getByPlaceholderText(/search/i);
-
     fireEvent.change(searchInput, {
       target: { value: 'sk8' }
     });
 
-    const result = await screen.findAllByText(/result for/i);
-    expect(result).toHaveLength(1);
-    expect(result[0]).toBeInTheDocument();
+    const result = await screen.findByText(/result for:/i);
+    expect(result).toBeInTheDocument();
 
     const animeResult = await screen.findAllByText(/sk8/i);
     expect(animeResult).toHaveLength(3);
